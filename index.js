b4be3f7e37608a5fc046dd567e1a6323cf709bcc
Rahsheen Porter <raporter@htt.io>
2017-09-10 18:24:21 -0400

    Added cors module

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const app = express()
 const router = require('./router')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // DB Setup
 mongoose.Promise = global.Promise
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://localhost/auth', {
 
 // App Setp
 app.use(morgan('combined'))
+app.use(cors())
 app.use(bodyParser.json({ type: '*/*' }))
 router(app)
 
